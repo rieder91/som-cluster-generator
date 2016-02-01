c624582be97568ac0e03fee99743db936ecaf851
Thomas Rieder <thomas@rieder.io>
2016-02-01 20:19:53 +0100

    translation of clusters

--- model/Cluster.py
+++ model/Cluster.py
@@ -1,11 +1,13 @@
 from model.Distribution import Distribution
-
+import operator
 
 class Cluster:
     def __init__(self, distribution=Distribution.gaussian, cardinality=5, density=5):
         self.distribution = distribution
         self.cardinality = cardinality
         self.density = density
+        self.values = []
+        self.center = ()
 
     def set_distribution(self, distribution):
         self.distribution = distribution
@@ -15,6 +17,21 @@ class Cluster:
 
     def set_density(self, density):
         self.density = density
+
+    def set_dimensions(self, dimensions):
+        self.center = tuple([0] * dimensions)
+
+    def set_values(self, values):
+        self.values = values
+
+    def translate_values(self, trans):
+        self.center = tuple(map(operator.add, self.center, trans))
+        for i in range(0, len(self.values)):
+            row = self.values[i]
+            new_row = ()
+            for j in range(0, len(row)):
+                new_row += (row[j] + trans[j],)
+            self.values[i] = new_row
 
     def __str__(self):
         return "distribution=%s, cardinality=%s, density=%s" % (self.distribution.name, self.cardinality, self.density)
--- model/Dataset.py
+++ model/Dataset.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 from model.rng.RNG import RNG
 from model.rng.RNGFactory import RNGFactory
 
@@ -6,7 +9,7 @@ class Dataset:
     def __init__(self, dimensions, clusters):
         self.dimensions = dimensions
         self.clusters = clusters
-        self.values = [None] * dimensions
+        self.translate_try_count = 2500
 
     def generate_values(self):
         for cluster in self.clusters:
@@ -22,13 +25,47 @@ class Dataset:
                     for i in range(0, self.dimensions):
                         row += (rng.get_next(),)
                     cluster_values.append(row)
-                self.values[self.clusters.index(cluster)] = cluster_values
+                cluster.set_values(cluster_values)
 
-                # TODO translation of cluster values
+    # translation of cluster values to distribute them evenly
+    def balance_clusters(self):
+        best_distance = 0
+        best_centers = [tuple([0] * self.dimensions)]
 
-                print(self.values)
+        for i in range(0, self.translate_try_count):
+            centers = []
+            for _ in self.clusters:
+                center = ()
+                for j in range(0, self.dimensions):
+                    center += (random.randint(-75, 75),)
+                centers.append(center)
+
+            if self.get_sum_of_distances(centers) > best_distance:
+                best_distance = self.get_sum_of_distances(centers)
+                best_centers = centers
+
+        for i in range(0, len(self.clusters)):
+            cluster = self.clusters[i]
+            cluster.translate_values(best_centers[i])
+
+    def get_sum_of_distances(self, centers):
+        sum = 0
+        for center_1 in centers:
+            for center_2 in centers:
+                if center_1 != center_2:
+                    sum += self.get_cluster_distance(center_1, center_2)
+        return sum
 
     @staticmethod
     def get_mapped_cardinality(cardinality):
-        # TODO map cardinality properly
-        return 100
+        # map cardinality
+        return 50 * pow(cardinality, 2)
+
+    @staticmethod
+    def get_cluster_distance(center_1, center_2):
+        # simple euclidean distance
+        root_sum = 0
+        for i in range(0, len(center_1)):
+            p, q = center_1[i], center_2[i]
+            root_sum += pow(q - p, 2)
+        return math.sqrt(root_sum)
