14dd6ca4e02c5f0ce8a433350a444c8b6641cebc
Thomas Rieder <thomas@rieder.io>
2016-02-07 20:33:22 +0100

    add bat file for generation

---
+++ generate.bat
@@ -0,0 +1,19 @@
+python generator.py input\same_cardinality_same_density_2d.json -q
+python generator.py input\same_cardinality_same_density_4d.json
+python generator.py input\same_cardinality_same_density_10d.json
+
+python generator.py input\same_cardinality_diff_density_2d.json -q
+python generator.py input\same_cardinality_diff_density_4d.json
+python generator.py input\same_cardinality_diff_density_10d.json
+
+python generator.py input\diff_cardinality_same_density_2d.json -q
+python generator.py input\diff_cardinality_same_density_4d.json
+python generator.py input\diff_cardinality_same_density_10d.json
+
+python generator.py input\diff_cardinality_diff_density_2d.json -q
+python generator.py input\diff_cardinality_diff_density_4d.json
+python generator.py input\diff_cardinality_diff_density_10d.json
+
+python generator.py input\distributions_2d.json -q
+python generator.py input\distributions_4d.json
+python generator.py input\distributions_10d.json
